301fc8d044aa72302ab61ddc7d349fd271fa371b
Aron Balogh <balogh.aron@gmail.com>
2022-06-20 15:37:20 +0200

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import './App.css';
-// import ldapjs from 'ldapjs';
-const ldap = require('ldapjs');
-
 function App() {
-  setTimeout(() => {
-    console.log('ldap :>> ', ldap);
-  }, 3000);
-
   return <div className='3Dapp'>3D fejlesztés</div>;
 }
 
