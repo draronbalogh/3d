3ae83b3f8c2ce544280694a63315a760ae9a938f
Aron Balogh <balogh.aron@gmail.com>
2022-12-13 23:58:04 +0100

    Added db exports

---
+++ db/3d_models_2022_12_13__2356 (1).sql
@@ -0,0 +1,73 @@
+CREATE DATABASE  IF NOT EXISTS `3d` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `3d`;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: 3d
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `models`
+--
+
+DROP TABLE IF EXISTS `models`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `models` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `modelUuid` varchar(36) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `modelUploaderUid` varchar(100) COLLATE utf8mb3_hungarian_ci DEFAULT NULL,
+  `modelTitle` varchar(1024) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `modelDescription` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `modelChannel` varchar(100) COLLATE utf8mb3_hungarian_ci DEFAULT NULL,
+  `modelCategory` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `modelFormat` varchar(100) COLLATE utf8mb3_hungarian_ci DEFAULT NULL,
+  `modelTags` varchar(2048) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `modelUrl` varchar(1024) COLLATE utf8mb3_hungarian_ci DEFAULT NULL,
+  `modelFileName` varchar(2048) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `modelImgs` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `modelSourceUrl` text CHARACTER SET utf8mb3 COLLATE utf8mb3_hungarian_ci,
+  `modelExtraLinks` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
+  `modelState` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `modelVisibility` tinyint(1) DEFAULT '1',
+  `modelRigged` tinyint(1) DEFAULT '0',
+  `modelAnimated` tinyint(1) DEFAULT '0',
+  `modelPolyCategory` varchar(100) COLLATE utf8mb3_hungarian_ci DEFAULT NULL,
+  `modelLegality` varchar(100) COLLATE utf8mb3_hungarian_ci DEFAULT 'Jogtiszta',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_hungarian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `models`
+--
+
+LOCK TABLES `models` WRITE;
+/*!40000 ALTER TABLE `models` DISABLE KEYS */;
+INSERT INTO `models` VALUES (1,'2022-12-12 22:59:30','2022-12-13 20:27:07',NULL,NULL,'rtve2020',NULL,NULL,NULL,NULL,NULL,'C:\\fakepath\\karakter_képek_2.png',NULL,NULL,NULL,NULL,NULL,1,0,0,'Közepes','Ingyenes modell'),(2,'2022-12-12 23:02:40','2022-12-12 23:54:15',NULL,NULL,'aaaaaaa',NULL,NULL,'Gép',NULL,NULL,'C:\\fakepath\\002.JPG',NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Nem ismert forrás'),(4,'2022-12-12 23:10:42','2022-12-12 23:38:17',NULL,NULL,'ssssssss','asd',NULL,'Gép',NULL,NULL,'C:\\fakepath\\minta1.png',NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta modell'),(5,'2022-12-12 23:54:45','2022-12-13 00:15:08',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'C:\\fakepath\\minta_002.png',NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Ingyenes modell'),(6,'2022-12-13 00:06:02','2022-12-13 00:18:31',NULL,NULL,NULL,'happy',NULL,NULL,NULL,NULL,'C:\\fakepath\\karakter_képek_2.png',NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta'),(7,'2022-12-13 00:19:16','2022-12-13 00:19:16',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,1,1,NULL,'Jogtiszta'),(8,'2022-12-13 00:19:30','2022-12-13 00:19:30',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta'),(9,'2022-12-13 00:19:45','2022-12-13 00:19:45',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,1,1,NULL,'Jogtiszta'),(10,'2022-12-13 00:19:56','2022-12-13 00:19:56',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,1,0,NULL,'Jogtiszta'),(11,'2022-12-13 00:20:01','2022-12-13 00:20:01',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,1,1,NULL,'Jogtiszta'),(12,'2022-12-13 00:20:43','2022-12-13 00:20:43',NULL,NULL,'ffffffffffff','ddddd',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta'),(15,'2022-12-13 20:43:02','2022-12-13 20:43:09',NULL,NULL,'ssssssssss',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta'),(16,'2022-12-13 21:17:15','2022-12-13 21:17:15',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,NULL,'Jogtiszta'),(17,'2022-12-13 21:17:33','2022-12-13 21:17:33',NULL,NULL,'aaaaaaaaaaaa',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta'),(18,'2022-12-13 21:17:50','2022-12-13 21:17:50',NULL,NULL,'zzzzzzz',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0,0,NULL,'Jogtiszta');
+/*!40000 ALTER TABLE `models` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-13 23:57:17
