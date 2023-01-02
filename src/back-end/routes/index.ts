eaac27821a39948fae69724827dd120e3487bcb7
Aron Balogh <balogh.aron@gmail.com>
2023-01-02 16:48:06 +0100

    Added multi upload

--- src/back-end/routes/index.ts
+++ src/back-end/routes/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+
 import { getAllModels3ds, createModels3d, getModels3dById, updateModels3d, delete3dModel } from '../controllers/controllers-3dmodels';
 const router = express.Router();
 router.get('/', getAllModels3ds);
@@ -6,4 +7,5 @@ router.get('/:id', getModels3dById);
 router.post('/', createModels3d);
 router.patch('/:id', updateModels3d);
 router.delete('/:id', delete3dModel);
+
 export default router;
--- src/back-end/server.ts
+++ src/back-end/server.ts
@@ -3,6 +3,9 @@ import db from '../_config/config-database';
 import routes3d from './routes/index';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import formidable from 'formidable';
+import os from 'node:os';
+import util from 'node:util';
 const app = express();
 const etst = async () => {
   await db.authenticate();
@@ -18,7 +21,40 @@ try {
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/upload', (req, res, next) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const form = formidable({ uploadDir: os.tmpdir() });
+  const files: any[] = [];
+  const fields: any[] = [];
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+
+  form
+    .on('field', (fieldName, value) => {
+      console.log(fieldName, value);
+      fields.push({ fieldName, value });
+    })
+    .on('file', (fieldName, file) => {
+      console.log(fieldName, file);
+      files.push({ fieldName, file });
+    })
+    .on('end', () => {
+      console.log('-> upload done');
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.write(`received fields:\n\n${util.inspect(fields)}`);
+      res.write('\n\n');
+      res.end(`received files:\n\n${util.inspect(files)}`);
+    });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      console.log('hiba');
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    res.json({ fields, files });
+  });
+});
 /*app.use(express.json());
 app.use(express.urlencoded({ extended: true }));*/
 app.use('/api/3dmodels', routes3d);
